0873c79ab6e2daa095ecdafb5eaac4d2e8059094
Efim Pyshnograev <efim@fastmail.com>
2021-11-30 17:57:51 +1100

    Formatting

--- client/client.cc
+++ client/client.cc
@@ -11,40 +11,40 @@ using grpc::ClientContext;
 using grpc::Status;
 
 class OumaClient {
-    private:
-        std::unique_ptr<OumaService::Stub> stub_;
-    public:
-        OumaClient(std::shared_ptr<Channel> channel) :
-            stub_(OumaService::NewStub(channel)) {}
+ private:
+  std::unique_ptr<OumaService::Stub> stub_;
+ public:
+  OumaClient(std::shared_ptr<Channel> channel) :
+      stub_(OumaService::NewStub(channel)) {}
 
-        std::string call_ouma(const std::string& input) {
-            OumaRequest request;
-            request.set_message(input);
+  std::string call_ouma(const std::string &input) {
+    OumaRequest request;
+    request.set_message(input);
 
-            OumaReply reply;
-            ClientContext context;
-            Status status = stub_->prophesize(&context, request, &reply);
+    OumaReply reply;
+    ClientContext context;
+    Status status = stub_->prophesize(&context, request, &reply);
 
-            if (status.ok()) {
-                return reply.message();
-            } else {
-                throw std::runtime_error("Failed to contact server");
-            }
-        }
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      throw std::runtime_error("Failed to contact server");
+    }
+  }
 };
 
-int main() {    
-    const std::string host {"localhost"};
-    const std::string port {"8888"};
-    const std::string address = host + ":" + port;
-    const std::string message = "itya";
+int main() {
+  const std::string host{"localhost"};
+  const std::string port{"8888"};
+  const std::string address = host + ":" + port;
+  const std::string message = "itya";
 
-    std::cout << "Sending the message " << message << " to " << address << std::endl;
+  std::cout << "Sending the message " << message << " to " << address << std::endl;
 
-    OumaClient client(grpc::CreateChannel(address, 
-        grpc::InsecureChannelCredentials()));
-    const std::string reply = client.call_ouma(message);
+  OumaClient client(grpc::CreateChannel(address,
+                                        grpc::InsecureChannelCredentials()));
+  const std::string reply = client.call_ouma(message);
 
-    std::cout << "Received a reply " << reply << std::endl;
-    return 0;
+  std::cout << "Received a reply " << reply << std::endl;
+  return 0;
 }
--- server/server.cc
+++ server/server.cc
@@ -10,27 +10,27 @@ using grpc::ServerBuilder;
 using grpc::Status;
 
 class OumaServiceImpl : public OumaService::Service {
-    Status prophesize(ServerContext* context, const OumaRequest* request, OumaReply* reply) {
-        const std::string input = request->message();
-        const std::string reply_string = "Hello, " + input;
-        reply->set_message(reply_string);
-        return Status::OK;
-    }
+  Status prophesize(ServerContext *context, const OumaRequest *request, OumaReply *reply) {
+    const std::string input = request->message();
+    const std::string reply_string = "Hello, " + input;
+    reply->set_message(reply_string);
+    return Status::OK;
+  }
 };
 
 int main() {
-    const std::string host {"localhost"};
-    const std::string port {"8888"};
-    const std::string address = host + ":" + port;
+  const std::string host{"localhost"};
+  const std::string port{"8888"};
+  const std::string address = host + ":" + port;
 
-    OumaServiceImpl service;
-    ServerBuilder builder;
-    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
+  OumaServiceImpl service;
+  ServerBuilder builder;
+  builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
 
-    std::unique_ptr<Server> server {builder.BuildAndStart()};
-    std::cout << "Shoopuf is waiting on " << address << std::endl;
+  std::unique_ptr<Server> server{builder.BuildAndStart()};
+  std::cout << "Shoopuf is waiting on " << address << std::endl;
 
-    server->Wait();
-    return 0;
+  server->Wait();
+  return 0;
 }
